8bec2203dbb1471226228d9bb0d13514d6a02f17
Pmayors <mayowalawal692@gmail.com>
2025-10-17 17:23:44 +0100

    Update code.sh

--- code.sh
+++ code.sh
@@ -5,6 +5,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # Install a network
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
+# Run this inside the master node
+sudo kubeadm init --apiserver-advertise-address=192.168.56.11 --pod-network-cidr=10.244.0.0/16
+
 #Generate token for worker nodes to join
 sudo kubeadm token create --print-join-command
 
